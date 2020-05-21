78f617c60f5f7d44ad271effb7c26ea5651322c5
gowniudaykiran <65489967+gowniudaykiran@users.noreply.github.com>
2020-05-21 17:07:27 +0530

    Update settings.py

--- libmgmt/settings.py
+++ libmgmt/settings.py
@@ -139,5 +139,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'gowniudaykiran@gmail.com'
-EMAIL_HOST_PASSWORD = 'tieetmkurbumkdgo'
+EMAIL_HOST_USER =os.environment.get('email')
+EMAIL_HOST_PASSWORD =os.environment.get('email_password')
